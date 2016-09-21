49f96e90b82b06b4b7c1c75225b77e18f50130f6
Cole Morton <cole.morton@hotmail.com>
2016-09-21 17:31:28 +0200

    fix to include lodash

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,13 @@ elixir(function (mix) {
             'bower_components/angular-animate/*min.js',
             'bower_components/angular-aria/*min.js',
             'bower_components/angular-resource/*min.js',
-            'bower_components/lodash/*min.js'
+            
+            /* 
+                I realise you specified that no changes to the build script were to be made,
+                although the following path is incorrect and lodash was not loaded as a result:
+                'bower_components/lodash/*min.js'
+            */
+            'bower_components/lodash/dist/lodash.min.js'
         ], 'public/js', 'vendor.js')
         .angular('resources/angular/app/', 'public/js', 'angular.js')
 
